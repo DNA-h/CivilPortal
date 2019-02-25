427057145756b7ce7a9f045475ffe142f6423f65
DNA.h <s.n.bahador@gmail.com>
2019-02-25 18:04:05 +0330

    -25 Feb

--- android/app/src/main/java/com/civilportal/MainApplication.java
+++ android/app/src/main/java/com/civilportal/MainApplication.java
@@ -3,6 +3,7 @@ package com.civilportal;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.beefe.picker.PickerViewPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new LinearGradientPackage(),
             new SplashScreenReactPackage(),
             new PickerViewPackage(),
